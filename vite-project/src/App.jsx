63a1d79aecd2e7147c2958f927404ef2ead32f79
Murilo <murilobauck2@gmail.com>
2025-03-20 23:45:06 -0300

    import cabecalho pelo js

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,14 @@
+import { Cabecalho } from "./components";
+
 import "./App.css";
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Cabecalho />
+    </>
+  );
 };
 
-export default App;
+export { App };
